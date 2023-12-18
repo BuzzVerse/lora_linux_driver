b25cf0a066106c398a76c5c692a659cbfbff78bd
Jacek Kowalski <jacekkowalski.zg@gmail.com>
2023-12-18 18:28:31 +0100

    Added frame structure for LoRa

---
+++ lora_frame.h
@@ -0,0 +1,12 @@
+#include <cstdint>
+#include <stdint.h>
+
+typedef struct lora_frame {
+    uint8_t MAC[6];
+    uint16_t packet_type;
+    uint8_t packet_number;
+    uint8_t packet_count;     
+    uint8_t data_count;       
+    uint8_t data[212];
+    uint8_t SHA256[32];
+}__attribute__((packed)) lora_frame;
