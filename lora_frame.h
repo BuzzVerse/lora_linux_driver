8c299c9c50d14f0ed24089b210bd8532dbae91a0
koperek <jan.ratajski@wp.pl>
2024-01-08 20:49:01 +0100

    Added SHA-256, untested

--- lora_frame.h
+++ lora_frame.h
@@ -4,8 +4,9 @@ typedef struct lora_frame {
     uint8_t MAC[6];
     uint16_t packet_type;
     uint8_t packet_number;
-    uint8_t packet_count;     
-    uint8_t data_count;       
+    uint8_t packet_count;
+    uint8_t data_count;
     uint8_t data[212];
     uint8_t SHA256[32];
-}__attribute__((packed)) lora_frame;
+} __attribute__((packed)) lora_frame;
+
---
+++ sha.h
@@ -0,0 +1,22 @@
+#ifndef SHA_H
+#define SHA_H
+
+#include "lora_frame.h"
+
+
+/*
+ * sha_verify
+ *
+ * Sprawdza, czy obliczona suma SHA-256 jest taka sama jak ta otrzymana
+ * w ramce.
+ *
+ * Parametry:
+ * frame: wczytana ramka LoRa
+ *
+ * Wartości zwracane:
+ * 0 = ok, SHA się zgadza
+ * 1 = są różne
+ */
+int sha_verify(lora_frame *frame);
+
+#endif
