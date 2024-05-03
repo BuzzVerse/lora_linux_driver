8fd638fda330644e1879e8406b0bb8ace5789d97
Jacek Kowalski <jacekkowalski.zg@gmail.com>
2024-05-03 23:55:43 +0200

    lora_utility.c: Added functions to set certain parameters in a more readable way

--- src/lora_utility.c
+++ src/lora_utility.c
@@ -18,6 +18,35 @@ void lora_initialize(int fd) {
     spi_write_register(fd, MODEM_CONFIG_2, 0xC4); // SF = 12, CRC enabled
 }
 
+void set_mode(int fd, uint8_t mode) {
+    spi_write_register(fd, OP_MODE, mode);
+}
+
+void set_bandwidth(int fd, int bw) {
+    if(bw < 0) { bw = 0; }
+    else if(bw > 9) { bw = 9; }
+    
+    spi_write_register(fd, MODEM_CONFIG_1, spi_read_register(fd, MODEM_CONFIG_1) | (bw << 4));
+}
+
+void set_coding_rate(int fd, int cr) {
+    if(cr < 1) { cr = 1; }
+    else if(cr > 4) { cr = 4; }
+
+    spi_write_register(fd, MODEM_CONFIG_1, spi_read_register(fd, MODEM_CONFIG_1) | (cr << 1));
+}
+
+void set_spreading_factor(int fd, int sf) {
+    if(sf < 6) { sf = 6; }
+    else if(sf > 12) { sf = 12; }
+
+    spi_write_register(fd, MODEM_CONFIG_2, spi_read_register(fd, MODEM_CONFIG_2) | (sf << 4));
+}
+
+void enable_crc(int fd) {
+    spi_write_register(fd, MODEM_CONFIG_2, spi_read_register(fd, MODEM_CONFIG_2) | (1 << 2));
+}
+
 void print_modem_status(int fd) {
     uint8_t modem_status = spi_read_register(fd, MODEM_STATUS);
 	sleep((double)0.1);
--- src/lora_utility.h
+++ src/lora_utility.h
@@ -1,7 +1,19 @@
 #ifndef LORA_UTILITY_H
 #define LORA_UTILITY_H
 
+#include <stdint.h>
+
 void lora_initialize(int fd);
+
+void set_mode(int fd, uint8_t mode);
+
+void set_bandwidth(int fd, int bw);
+
+void set_coding_rate(int fd, int cr);
+
+void set_spreading_factor(int fd, int sf);
+
+void enable_crc(int fd);
 
 void print_modem_status(int fd);
 
