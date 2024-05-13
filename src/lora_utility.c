58d06f502595c992c3c03334f40f66d484c2d5ff
DabP03 <piotrek.dabrowa@gmail.com>
2024-05-13 19:08:41 +0200

    lora_reset works

--- src/lora_utility.c
+++ src/lora_utility.c
@@ -189,7 +189,6 @@ void lora_delay(uint32_t ticks) {
     //vTaskDelay(ticks);    
 }
 
-// TODO fix
 void lora_reset(void) {
     FILE *fptr66, *fptr69;
     fptr66 = fopen("/sys/class/gpio/gpio66/value", "w");
@@ -200,30 +199,18 @@ void lora_reset(void) {
         return; 
     }
 
-    fprintf(fptr66, "%d", 0);
-    fprintf(fptr69, "%d", 0);
+    fseek(fptr66, 0, SEEK_SET);
+    fseek(fptr69, 0, SEEK_SET);
 
-    fclose(fptr66);
-    fclose(fptr69);
+    for (int i = 0; i < 2; i++) {
+        fprintf(fptr66, "%d", i);
+        fflush(fptr66);
 
+        fprintf(fptr69, "%d", i);
+        fflush(fptr69);
 
-    sleep((double)(0.001));
-
-    fptr66 = fopen("/sys/class/gpio/gpio66/value", "w");
-    fptr69 = fopen("/sys/class/gpio/gpio69/value", "w");
-
-    if ((fptr66 == NULL) || (fptr69 == NULL)) {
-        printf("%s[Error]%s Reading GPIO failed.\n", C_RED, C_DEFAULT);
-        return; 
+        sleep(1);
     }
-
-    fprintf(fptr66, "%d", 1);
-    fprintf(fptr69, "%d", 1);
-
-    fclose(fptr66);
-    fclose(fptr69);
-
-    sleep((double)(0.005));
-
     printf("%s[RESET]%s Ok\n", C_GREEN, C_DEFAULT);
 }
+// TODO fix
