149fe2fb8024efddd62ceaf43b82d0b2fd18a356
DabP03 <piotrek.dabrowa@gmail.com>
2024-05-13 18:57:35 +0200

    test added fix 8

--- src/test.c
+++ src/test.c
@@ -13,30 +13,27 @@ void lora_reset(void) {
         return; 
     }
 
+    fseek(fptr66, 0, SEEK_SET);
+    fseek(fptr69, 0, SEEK_SET);
+
     fprintf(fptr66, "%d", 0);
+    fflush(fptr66);
+
     fprintf(fptr69, "%d", 0);
+    fflush(fptr69);
 
-    fclose(fptr66);
-    fclose(fptr69);
-
-
-    sleep((double)(0.001));
-
-    fptr66 = fopen("/sys/class/gpio/gpio66/value", "w");
-    fptr69 = fopen("/sys/class/gpio/gpio69/value", "w");
-
-    if ((fptr66 == NULL) || (fptr69 == NULL)) {
-        printf("%s[Error]%s Reading GPIO failed.\n", C_RED, C_DEFAULT);
-        return; 
-    }
+    sleep(1);
 
     fprintf(fptr66, "%d", 1);
+    fflush(fptr66);
+
     fprintf(fptr69, "%d", 1);
+    fflush(fptr69);
 
     fclose(fptr66);
     fclose(fptr69);
 
-    sleep((double)(0.005));
+    sleep(1);
 
     printf("%s[RESET]%s Ok\n", C_GREEN, C_DEFAULT);
 }
