817232eceb3b73b685edfc1f05b78c916ae4b84f
DabP03 <piotrek.dabrowa@gmail.com>
2024-05-13 18:37:54 +0200

    test added fix 5

--- src/test.c
+++ src/test.c
@@ -4,11 +4,11 @@
 #include "colors.h"
 
 void lora_reset(void) {
-    system("sudo echo 0 > /sys/class/gpio/gpio66/value");
-    system("sudo echo 0 > /sys/class/gpio/gpio69/value");
+    system("sudo sh -c 'echo 0 > /sys/class/gpio/gpio66/value'");
+    system("sudo sh -c 'echo 0 > /sys/class/gpio/gpio69/value'");
     system("sleep .0001s");
-    system("sudo echo 1 > /sys/class/gpio/gpio66/value");
-    system("sudo echo 1 > /sys/class/gpio/gpio69/value");
+    system("sudo sh -c 'echo 1 > /sys/class/gpio/gpio66/value'");
+    system("sudo sh -c 'echo 1 > /sys/class/gpio/gpio69/value'");
     system("sleep .005s");
 }
 
