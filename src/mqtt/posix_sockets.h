a163d94738a71b35ef34dc0e70ebba2907dbefd0
DabP03 <piotrek.dabrowa@gmail.com>
2024-06-28 17:34:41 +0200

    mqtt should work

--- src/mqtt/posix_sockets.h
+++ src/mqtt/posix_sockets.h
@@ -4,15 +4,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#if !defined(WIN32)
 #include <sys/socket.h>
 #include <netdb.h>
-#else
-#include <ws2tcpip.h>
-#endif
-#if defined(__VMS)
-#include <ioctl.h>
-#endif
 #include <fcntl.h>
 
 /*
@@ -29,12 +22,12 @@ int open_nb_socket(const char* addr, const char* port) {
     int rv;
     struct addrinfo *p, *servinfo;
 
-    /* get address information */
-    rv = getaddrinfo(addr, port, &hints, &servinfo);
-    if(rv != 0) {
-        fprintf(stderr, "Failed to open socket (getaddrinfo): %s\n", gai_strerror(rv));
-        return -1;
-    }
+    // /* get address information */
+    // rv = getaddrinfo(addr, port, &hints, &servinfo);
+    // if(rv != 0) {
+    //     fprintf(stderr, "Failed to open socket (getaddrinfo): %s\n", gai_strerror(rv));
+    //     return -1;
+    // }
 
     /* open the first possible socket */
     for(p = servinfo; p != NULL; p = p->ai_next) {
@@ -55,23 +48,7 @@ int open_nb_socket(const char* addr, const char* port) {
     freeaddrinfo(servinfo);
 
     /* make non-blocking */
-#if !defined(WIN32)
     if (sockfd != -1) fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL) | O_NONBLOCK);
-#else
-    if (sockfd != INVALID_SOCKET) {
-        int iMode = 1;
-        ioctlsocket(sockfd, FIONBIO, &iMode);
-    }
-#endif
-#if defined(__VMS)
-    /* 
-        OpenVMS only partially implements fcntl. It works on file descriptors
-        but silently fails on socket descriptors. So we need to fall back on
-        to the older ioctl system to set non-blocking IO
-    */
-    int on = 1;                 
-    if (sockfd != -1) ioctl(sockfd, FIONBIO, &on);
-#endif
 
     /* return the new socket fd */
     return sockfd;
